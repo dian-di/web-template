c01a6beed8aa3d69ad84a45c7aad77fc8922fef6
yang <1203588983@qq.com>
2025-07-31 09:33:23 +0800

    update biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
-import { Button } from "@/components/ui/button"
-import { XIcon } from "lucide-react"
-
+import { XIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 function App() {
-
   return (
     <div className='flex min-h-svh flex-col items-center justify-center'>
       <Button className='text-red-500'>Click me</Button>
-      <XIcon className="size-4" />
+      <XIcon className='size-4' />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import path from "node:path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import biomePlugin from 'vite-plugin-biome'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), biomePlugin()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
